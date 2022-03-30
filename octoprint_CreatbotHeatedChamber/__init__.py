cf25b7d41b70df56e5827ff98bf8985cdf01c5a9
Kestin Goforth <kgoforth1503@gmail.com>
2022-03-30 11:29:01 -0400

    Initial Upload

---
+++ octoprint_CreatbotHeatedChamber/__init__.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import octoprint.plugin
+
+class CreatbotChamberCommandPlugin(octoprint.plugin.StartupPlugin):
+    def on_after_startup(self):
+        self._logger.info("Starting Creatbot Heated Chamber Command Plugin")
+
+    def sending_gcode_hook(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+        if gcode and gcode == "M141":
+            cmd = "M6013" + cmd[4:]
+            self._logger.info(f"Replacing M141 command with M6013: {cmd}")
+        return cmd,
+
+
+__plugin_name__ = "Creatbot Heated Chamber"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "Replace the default Marlin heated build volume command (M141) with the Creabot version (M6013)."
+__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_implementation__ = CreatbotChamberCommandPlugin()
+__plugin_hooks__ = {
+    "octoprint.comm.protocol.gcode.sending": __plugin_implementation__.sending_gcode_hook,
+}
+
+"""
+Other Creatbot GCode Commands:
+ * M6001 - Pause print same as M600 but not retracting filament.
+ * M6002 - Resume print from M6001.
+ * M6003 - Unload filament after detect filament broken
+ * M6004 - Resume print from accident
+ * M6005 - Home & Resume print from accident
+ * M6006 - Start Serial print
+ * M6007 - Stop Serial print
+ * M6008 - Set used Time											(M6008 S0.00)
+ * M6009 - Set regSN												(M6009 S0.00)
+ * M6010 - Adjust the Z Value after adjust zprobe_zoffset		    (Requires HAS_BED_PROBE)
+ * M6011 - (like M106) Set Temp fan.								(Requires HAS_AUTO_FAN)
+ * M6012 - (like M106) Set Air fan.									(Requires HAS_AIR_FAN)
+ * M6013 - (like M140) Set Chamber Temp. 							(Requires HOTWIND_SYSTEM)
+ * M6014 - (include G29) Probe the bed.								(Requires HAS_LEVELING)
+ * M6020 - (like M20) List USB
+ * M6021 - (like M21) Init USB
+ * M6022 - (like M22) Release USB
+ * M6023 - (like M23) Select USB file
+ * M6024 - (like M24) Start/resume USB print
+ * M6025 - (like M25) Pause USB print
+ * M6026 - (like M26) Set USB position in bytes
+ * M6027 - (like M27) Report USB print status
+ * M6028 - (like M28) Start USB write.								TODO M6028
+ * M6029 - (like M29) Stop USB write.								TODO M6029
+ * M6030 - (like M30) Delete file from USB.						    TODO M6030
+ * M6032 - (like M32) Select file and start SD print
+ * M6033 - (like M928) Start USB write (logging)			        TODO M6033
+"""
